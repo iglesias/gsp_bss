84c9e6218b1634da0bb211bbd40ddad930c62269
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-06-13 17:45:12 +0200

    Add filter construction and deconvolution in source separation.
    With one filter. Next we extend it to use both filters.

--- start_ss.m
+++ start_ss.m
@@ -12,12 +12,29 @@ G.L = diag(sum(G.W))-G.W;
 assert(issymmetric(G.W))
 assert(issymmetric(G.L))
 
-figure
-matlabGraph = graph(G.W);
-plot(matlabGraph)
+%figure
+%matlabGraph = graph(G.W);
+%plot(matlabGraph)
 
 [V, D] = eig(G.L);
 lambda = diag(D);
 
-showFlag = 1;
-gsp_design_filters(lambda, 10, showFlag);
+showFlag = 0;
+numFilterCoeffs = 10;
+[hLP, hHP] = gsp_design_filters(lambda, numFilterCoeffs, showFlag);
+
+% Build filter matrix.
+H = hLP(1)*eye(N);
+for l = 1:numFilterCoeffs-1
+  H = H + hLP(l+1)*G.L^l;
+end
+
+% Input.
+x = zeros(N, 1);
+% Number of non-zero input nodes.
+S = 20;
+xSupport = randperm(N, S);
+x(xSupport) = rand(S, 1);
+
+y = H*x;
+xest = H\y;
