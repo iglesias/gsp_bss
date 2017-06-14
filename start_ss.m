600a81c658b805bc4915d32ded9b5c7c8288c338
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-06-14 17:42:23 +0200

    Extend source seperation to two input signals with known support.

--- start_ss.m
+++ start_ss.m
@@ -23,18 +23,42 @@ showFlag = 0;
 numFilterCoeffs = 10;
 [hLP, hHP] = gsp_design_filters(lambda, numFilterCoeffs, showFlag);
 
-% Build filter matrix.
-H = hLP(1)*eye(N);
+% Build filter matrices.
+H1 = hLP(1)*eye(N);
 for l = 1:numFilterCoeffs-1
-  H = H + hLP(l+1)*G.L^l;
+  H1 = H1 + hLP(l+1)*G.L^l;
 end
 
+H2 = hHP(1)*eye(N);
+for l = 1:numFilterCoeffs-1
+  H2 = H2 + hHP(l+1)*G.L^l;
+end
+
+H = [H1 H2];
+
+I = eye(N);
+
 % Input.
-x = zeros(N, 1);
 % Number of non-zero input nodes.
 S = 20;
-xSupport = randperm(N, S);
-x(xSupport) = rand(S, 1);
 
+x1 = zeros(N, 1);
+x1Support = randperm(N, S);
+x1(x1Support) = rand(S, 1);
+Cs1 = I(x1Support, :);
+
+x2 = zeros(N, 1);
+x2Support = randperm(N, S);
+x2(x2Support) = rand(S, 1);
+Cs2 = I(x2Support, :);
+
+x = [x1; x2];
 y = H*x;
-xest = H\y;
+xnzest = [H1*Cs1' H2*Cs2']\y;
+
+x1est = zeros(N, 1);
+x1est(x1Support) = xnzest(1:S);
+x2est = zeros(N, 1);
+x2est(x2Support) = xnzest(S+1:end);
+
+sum(abs(x - [x1est; x2est]))
