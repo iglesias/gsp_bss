5e5d0923dbba8dc319ea10e0dca3e6081c34d810
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-06-21 17:30:09 +0200

    Rename bss script. Add comfy way to change to bss with known input support.

--- bss.m
+++ bss.m
@@ -56,16 +56,24 @@ x2(xSupport) = rand(S, 1);
 x = [x1; x2];
 y = H*x;
 
-Ubar = U(:, xSupport);
-A = kr(Psi', Ubar')';
+knownSupportFlag = false;
 
-Z1 = x1(xSupport)*hLP';
-Z2 = x2(xSupport)*hHP';
-knownSupportFlag = 1;
-[Z1est, Z2est] = sparse_bss_nuclear(y, A, V, 1e-1, 0, knownSupportFlag, S);
+if knownSupportFlag
+  % Known support equal for all inputs
+  % (see (3) in CAMSAP_BlindId for the expression below in the single-input case).
+  Ubar = U(:, xSupport);
+  A = kr(Psi', Ubar')';
+  xSupportToEstimate = xSupport;
+else
+  A = kr(Psi', U')';
+  xSupportToEstimate = 1:N;
+end
 
-[Z1 Z2]
-[Z1est Z2est]
+[Z1est, Z2est] = sparse_bss_nuclear(y, A, V, 1e-1, 0, knownSupportFlag);
 
-[Z1+Z2]
-[Z1est+Z2est]
+Z1 = x1(xSupportToEstimate)*hLP';
+Z2 = x2(xSupportToEstimate)*hHP';
+norm(Z1est - Z1)
+norm(Z2est - Z2)
+norm([Z1+Z2] - [Z1est+Z2est])
+norm(Z1est - Z2est)
