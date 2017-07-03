c38c50d2fad16292bbfa15d8a4005bcd8cd2061e
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2017-07-03 23:33:48 +0200

    Update bss to a smaller problem.
    + Pretty printing of problem statistics.
    + Plotting of results.

--- bss.m
+++ bss.m
@@ -1,7 +1,7 @@
 clearvars
 
 % Number of nodes.
-N = 100;
+N = 50;
 % Edge existence probability.
 p = 0.1;
 % Adjacency matrix.
@@ -20,11 +20,15 @@ assert(issymmetric(G.L))
 U = inv(V);
 lambda = diag(D);
 
-showFlag = 0;
-numFilterCoeffs = 15;
-[hLP, hHP] = gsp_design_filters(lambda, numFilterCoeffs, showFlag);
+%showFlag = 0;
+%numFilterCoeffs = 15;
+%[hLP, hHP] = gsp_design_filters(lambda, numFilterCoeffs, showFlag);
 
-Psi = repmat(lambda, 1, numFilterCoeffs).^[0:numFilterCoeffs-1];
+numFilterCoeffs = 2;
+hLP = [0 1]';
+hHP = [-1 0]';
+
+Psi = repmat(lambda, 1, numFilterCoeffs).^repmat([0:numFilterCoeffs-1], N, 1);
 
 % Build filter matrices.
 H1 = hLP(1)*eye(N);
@@ -43,15 +47,16 @@ I = eye(N);
 
 % Input.
 % Number of non-zero input nodes.
-S = 10;
+S = 5;
 
-xSupport = randperm(N, S);
+x1Support = randperm(N, S);
+x2Support = randperm(N, S);
 
 x1 = zeros(N, 1);
-x1(xSupport) = rand(S, 1);
+x1(x1Support) = rand(S, 1);
 
 x2 = zeros(N, 1);
-x2(xSupport) = rand(S, 1);
+x2(x2Support) = rand(S, 1);
 
 x = [x1; x2];
 y = H*x;
@@ -73,7 +78,22 @@ end
 
 Z1 = x1(xSupportToEstimate)*hLP';
 Z2 = x2(xSupportToEstimate)*hHP';
-norm(Z1est - Z1)
-norm(Z2est - Z2)
-norm([Z1+Z2] - [Z1est+Z2est])
-norm(Z1est - Z2est)
+
+fprintf('\n\n')
+fprintf('norm(Z1est-Z1)=%d\n', norm(Z1est - Z1))
+fprintf('norm(Z2est-Z2)=%d\n', norm(Z2est - Z2))
+fprintf('norm([Z1+Z2]-[Z1est+Z2est])=%d\n', norm([Z1+Z2] - [Z1est+Z2est]))
+fprintf('norm(Z1-Z2)=%d\n', norm(Z1 - Z2))
+fprintf('norm(Z1est-Z2est)=%d\n', norm(Z1est - Z2est))
+fprintf('norm_nuc(Z1)+norm_nuc(Z2)=%d\n', norm_nuc(Z1)+norm_nuc(Z2))
+fprintf('norm_nuc(Z1est)+norm_nuc(Z2est)=%d\n', norm_nuc(Z1est)+norm_nuc(Z2est))
+fprintf('\n\n')
+
+subplot(221)
+imagesc(Z1), title('Z1')
+subplot(222)
+imagesc(Z2), title('Z2')
+subplot(223)
+imagesc(Z1est), title('Z1est')
+subplot(224)
+imagesc(Z2est), title('Z2est')
