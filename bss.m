d6732794a60a261a6aff03e33c4086a20493ac05
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2017-07-18 23:03:24 +0200

    Add verbose option to bss.
    This way we finally avoid commenting/un-commenting all the fprintf.

--- bss.m
+++ bss.m
@@ -1,4 +1,4 @@
-function bss(taux, tauh)
+function bss(taux, tauh, verbose)
 
 if nargin < 1
   taux = 1e-1;
@@ -6,6 +6,10 @@ end
 
 if nargin < 2
   tauh = 1e-1;
+end
+
+if nargin < 3
+  verbose = false;
 end
 
 % Number of nodes.
@@ -82,10 +86,10 @@ else
   xSupportToEstimate = 1:N;
 end
 
-verbose = false;
-[Z1_hat, Z2_hat] = sparse_bss_nuclear(y, A, V, taux, tauh, verbose, knownSupportFlag);
-%[Z1_hat, Z2_hat] = sparse_bss_logdet(y, A, V, taux, tauh, verbose, knownSupportFlag);
-%[Z1_hat, Z2_hat] = sparse_bss_logdet_jointsum(y, A, V, 1e-1, 0, verbose, knownSupportFlag);
+sparse_bss_verbose = false;
+[Z1_hat, Z2_hat] = sparse_bss_nuclear(y, A, V, taux, tauh, sparse_bss_verbose, knownSupportFlag);
+%[Z1_hat, Z2_hat] = sparse_bss_logdet(y, A, V, taux, tauh, sparse_bss_verbose, knownSupportFlag);
+%[Z1_hat, Z2_hat] = sparse_bss_logdet_jointsum(y, A, V, 1e-1, 0, sparse_bss_verbose, knownSupportFlag);
 
 Z1 = x1(xSupportToEstimate)*hLP';
 Z2 = x2(xSupportToEstimate)*hHP';
@@ -96,30 +100,34 @@ save(sprintf('data/tuneando/bss_nuclear_tuneando_10_01_%s', randomstring(20)))
 %h1FromZ1 = sqrt(Sz1(1,1))*Uz1(:,1);
 %x1FromZ1 = sqrt(Sz1(1,1))*Vz1(:,1);
 
-%fprintf('\n\n')
-%fprintf('Equality constraint test: %d\n', norm(y - V*A*(Z1_hat(:) + Z2_hat(:))))
-%fprintf('norm(Z1)=%d\n', norm(Z1))
-%fprintf('norm(Z1_hat)=%d\n', norm(Z1_hat))
-%fprintf('norm(Z1_hat-Z1)=%d\n', norm(Z1_hat - Z1))
-%fprintf('norm(Z2)=%d\n', norm(Z2))
-%fprintf('norm(Z2_hat)=%d\n', norm(Z2_hat))
-%fprintf('norm(Z2_hat-Z2)=%d\n', norm(Z2_hat - Z2))
-%fprintf('norm([Z1+Z2]-[Z1_hat+Z2_hat])=%d\n', norm([Z1+Z2] - [Z1_hat+Z2_hat]))
-%fprintf('norm(Z1-Z2)=%d\n', norm(Z1 - Z2))
-%fprintf('norm(Z1_hat-Z2_hat)=%d\n', norm(Z1_hat - Z2_hat))
-%fprintf('norm_nuc(Z1)+norm_nuc(Z2)=%d\n', norm_nuc(Z1)+norm_nuc(Z2))
-%fprintf('norm_nuc(Z1_hat)+norm_nuc(Z2_hat)=%d\n', norm_nuc(Z1_hat)+norm_nuc(Z2_hat))
-%fprintf('\n\n')
-%
-%cminmax = minmax([Z1(:); Z2(:); Z1_hat(:); Z2_hat(:)]);
-%
-%subplot(221)
-%imagesc(Z1, cminmax), title('Z1')
-%subplot(222)
-%imagesc(Z2, cminmax), title('Z2')
-%subplot(223)
-%imagesc(Z1_hat, cminmax), title('Z1\_hat')
-%subplot(224)
-%imagesc(Z2_hat, cminmax), title('Z2\_hat')
+if verbose
+  fprintf('\n\n')
+  fprintf('Equality constraint test: %d\n', norm(y - V*A*(Z1_hat(:) + Z2_hat(:))))
+  fprintf('norm(Z1)=%d\n', norm(Z1))
+  fprintf('norm(Z1_hat)=%d\n', norm(Z1_hat))
+  fprintf('norm(Z1_hat-Z1)=%d\n', norm(Z1_hat - Z1))
+  fprintf('norm(Z2)=%d\n', norm(Z2))
+  fprintf('norm(Z2_hat)=%d\n', norm(Z2_hat))
+  fprintf('norm(Z2_hat-Z2)=%d\n', norm(Z2_hat - Z2))
+  fprintf('norm([Z1+Z2]-[Z1_hat+Z2_hat])=%d\n', norm([Z1+Z2] - [Z1_hat+Z2_hat]))
+  fprintf('norm(Z1-Z2)=%d\n', norm(Z1 - Z2))
+  fprintf('norm(Z1_hat-Z2_hat)=%d\n', norm(Z1_hat - Z2_hat))
+  fprintf('norm_nuc(Z1)+norm_nuc(Z2)=%d\n', norm_nuc(Z1)+norm_nuc(Z2))
+  fprintf('norm_nuc(Z1_hat)=%d\n', norm_nuc(Z1_hat))
+  fprintf('norm_nuc(Z2_hat)=%d\n', norm_nuc(Z2_hat))
+  fprintf('norm_nuc(Z1_hat)+norm_nuc(Z2_hat)=%d\n', norm_nuc(Z1_hat)+norm_nuc(Z2_hat))
+  fprintf('\n\n')
+
+  cminmax = minmax([Z1(:); Z2(:); Z1_hat(:); Z2_hat(:)]);
+
+  subplot(221)
+  imagesc(Z1, cminmax), title('Z1')
+  subplot(222)
+  imagesc(Z2, cminmax), title('Z2')
+  subplot(223)
+  imagesc(Z1_hat, cminmax), title('Z1\_hat')
+  subplot(224)
+  imagesc(Z2_hat, cminmax), title('Z2\_hat')
+end
 
 end
