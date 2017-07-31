ba65c5a2e6afa79dfeb97ba67b35e403a3a45ac1
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-07-31 10:10:16 +0200

    Move problem generation out of bss to own script.

--- bss.m
+++ bss.m
@@ -12,87 +12,12 @@ if nargin < 3
   verbose = false;
 end
 
-% Number of nodes.
-N = 50;
-% Edge existence probability.
-p = 0.1;
-% Adjacency matrix.
-G.W = generate_connected_ER(N, p);
-% Graph Laplacian.
-G.L = diag(sum(G.W))-G.W;
-
-assert(issymmetric(G.W))
-assert(issymmetric(G.L))
-
-%figure
-%matlabGraph = graph(G.W);
-%plot(matlabGraph)
-
-[V, D] = eig(G.L);
-U = inv(V);
-lambda = diag(D);
-
-%showFlag = 0;
-%numFilterCoeffs = 15;
-%[hLP, hHP] = gsp_design_filters(lambda, numFilterCoeffs, showFlag);
-
-numFilterCoeffs = 2;
-hLP = [1 0]';
-hHP = [0 1]';
-
-Psi = repmat(lambda, 1, numFilterCoeffs).^repmat([0:numFilterCoeffs-1], N, 1);
-
-% Build filter matrices.
-H1 = hLP(1)*eye(N);
-for l = 1:numFilterCoeffs-1
-  H1 = H1 + hLP(l+1)*G.L^l;
-end
-
-H2 = hHP(1)*eye(N);
-for l = 1:numFilterCoeffs-1
-  H2 = H2 + hHP(l+1)*G.L^l;
-end
-
-H = [H1 H2];
-
-I = eye(N);
-
-% Input.
-% Number of non-zero input nodes.
-S = 5;
-
-x1Support = randperm(N, S);
-x2Support = randperm(N, S);
-
-x1 = zeros(N, 1);
-x1(x1Support) = rand(S, 1);
-
-x2 = zeros(N, 1);
-x2(x2Support) = rand(S, 1);
-
-x = [x1; x2];
-y = H*x;
-
-knownSupportFlag = false;
-
-if knownSupportFlag
-  % Known support equal for all inputs
-  % (see (3) in CAMSAP_BlindId for the expression below in the single-input case).
-  Ubar = U(:, xSupport);
-  A = kr(Psi', Ubar')';
-  xSupportToEstimate = xSupport;
-else
-  A = kr(Psi', U')';
-  xSupportToEstimate = 1:N;
-end
+bss_gen_problem
 
 sparse_bss_verbose = false;
 [Z1_hat, Z2_hat] = sparse_bss_nuclear(y, A, V, taux, tauh, sparse_bss_verbose, knownSupportFlag);
 %[Z1_hat, Z2_hat] = sparse_bss_logdet(y, A, V, taux, tauh, sparse_bss_verbose, knownSupportFlag);
 %[Z1_hat, Z2_hat] = sparse_bss_logdet_jointsum(y, A, V, 1e-1, 0, sparse_bss_verbose, knownSupportFlag);
-
-Z1 = x1(xSupportToEstimate)*hLP';
-Z2 = x2(xSupportToEstimate)*hHP';
 
 save(sprintf('data/tuneando/bss_nuclear_tuneando_10_01_%s', randomstring(20)))
 
---
+++ bss_gen_problem.m
@@ -0,0 +1,74 @@
+% Number of nodes.
+N = 50;
+% Edge existence probability.
+p = 0.1;
+% Adjacency matrix.
+G.W = generate_connected_ER(N, p);
+% Graph Laplacian.
+G.L = diag(sum(G.W))-G.W;
+
+assert(issymmetric(G.W))
+assert(issymmetric(G.L))
+
+%figure
+%matlabGraph = graph(G.W);
+%plot(matlabGraph)
+
+[V, D] = eig(G.L);
+U = inv(V);
+lambda = diag(D);
+
+numFilterCoeffs = 2;
+hLP = rand(2, 1);
+hHP = rand(2, 1);
+
+Psi = repmat(lambda, 1, numFilterCoeffs).^repmat([0:numFilterCoeffs-1], N, 1);
+
+% Build filter matrices.
+H1 = hLP(1)*eye(N);
+for l = 1:numFilterCoeffs-1
+  H1 = H1 + hLP(l+1)*G.L^l;
+end
+
+H2 = hHP(1)*eye(N);
+for l = 1:numFilterCoeffs-1
+  H2 = H2 + hHP(l+1)*G.L^l;
+end
+
+H = [H1 H2];
+
+I = eye(N);
+
+% Input.
+% Number of non-zero input nodes.
+S = 5;
+
+x1Support = randperm(N, S);
+x2Support = randperm(N, S);
+
+%intersect(x1Support, x2Support)
+
+x1 = zeros(N, 1);
+x1(x1Support) = rand(S, 1);
+
+x2 = zeros(N, 1);
+x2(x2Support) = rand(S, 1);
+
+x = [x1; x2];
+y = H*x;
+
+knownSupportFlag = false;
+
+if knownSupportFlag
+  % Known support equal for all inputs
+  % (see (3) in CAMSAP_BlindId for the expression below in the single-input case).
+  Ubar = U(:, xSupport);
+  A = kr(Psi', Ubar')';
+  xSupportToEstimate = xSupport;
+else
+  A = kr(Psi', U')';
+  xSupportToEstimate = 1:N;
+end
+
+Z1 = x1(xSupportToEstimate)*hLP';
+Z2 = x2(xSupportToEstimate)*hHP';
