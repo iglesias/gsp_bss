6f338654c783f9566b19138372d162480d844cfa
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2017-09-12 23:25:09 +0200

    Minor style edits.

--- wrapper_twograph_bss_logdet.m
+++ wrapper_twograph_bss_logdet.m
@@ -4,7 +4,11 @@ verbose_twograph_bss_logdet = false;
 verbose_self = true;
 
 [truth, model, y] = twograph_bss_gen_problem;
-[Z1_hat, Z2_hat] = twograph_bss_logdet(y, model.A1, model.G(1).V, model.A2, model.G(2).V, verbose_twograph_bss_logdet);
-if verbose_self, twograph_bss_print_summary(Z1_hat, Z2_hat, truth, model, y), end
+[Z1_hat, Z2_hat] = twograph_bss_logdet(y, model.A1, model.G(1).V, model.A2, ...
+                                       model.G(2).V, ...
+                                       verbose_twograph_bss_logdet);
+if verbose_self
+  twograph_bss_print_summary(Z1_hat, Z2_hat, truth, model, y)
+end
 
 end
