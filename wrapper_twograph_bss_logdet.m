c68eb72977983d1c3fdc6f8bbce7e3b13bf3ac51
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-09-13 09:19:49 +0200

    Add plot to wrapper.

--- wrapper_twograph_bss_logdet.m
+++ wrapper_twograph_bss_logdet.m
@@ -2,9 +2,14 @@ function wrapper_twograph_bss_logdet
 
 verbose_twograph_bss_logdet = false;
 verbose_self = true;
+do_plot = false;
 
 [truth, model, y] = twograph_bss_gen_problem;
 [Z1_hat, Z2_hat] = twograph_bss_logdet(y, model.A1, model.G(1).V, model.A2, model.G(2).V, verbose_twograph_bss_logdet);
 if verbose_self, twograph_bss_print_summary(Z1_hat, Z2_hat, truth, model, y), end
+
+if do_plot
+  bss_plot_results(truth.Z1, truth.Z2, Z1_hat, Z2_hat);
+end
 
 end
