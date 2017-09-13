0d3bb8c33f0149b57f03c36699a6402f2d9b56cb
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-09-13 09:29:14 +0200

    Function-ize plot results.

--- bss_plot_results.m
+++ bss_plot_results.m
@@ -1,3 +1,5 @@
+function bss_plot_results(Z1, Z2, Z1_hat, Z2_hat)
+
 cminmax = minmax([Z1(:); Z2(:); Z1_hat(:); Z2_hat(:)]');
 
 figure(1)
@@ -11,4 +13,4 @@ imagesc(Z1_hat, cminmax), title('Z1\_hat')
 subplot(224)
 imagesc(Z2_hat, cminmax), title('Z2\_hat')
 
-clear cminmax
+end
