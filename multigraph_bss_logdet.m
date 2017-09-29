e4e6c4936efb358060540f35f578c61b93b14946
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-09-30 00:20:31 +0200

    Minor: reindent.

--- multigraph_bss_logdet.m
+++ multigraph_bss_logdet.m
@@ -29,7 +29,7 @@ iter = 1;
 Theta_old = repmat(eye(N), [1, 1, numGraphs]);
 Kappa_old = repmat(eye(L), [1, 1, numGraphs]);
 
-% Find initial values for Z1 and Z2.
+% Find initial values.
 Z_old = multigraph_bss_nuclear_direct(y, A, V, verbose);
 
 % Majorization-minimization
@@ -45,33 +45,33 @@ while (flag == 1 && iter <= max_iter)
   end
 
   cvx_begin quiet
-    variable Z(N, L, numGraphs);
-    variable Theta(N, N, numGraphs) symmetric;
-    variable Kappa(L, L, numGraphs) symmetric;
+  variable Z(N, L, numGraphs);
+  variable Theta(N, N, numGraphs) symmetric;
+  variable Kappa(L, L, numGraphs) symmetric;
 
-    pho = 1;
-    tau = 0.5;
-    objective = 0;
-    for i = 1:numGraphs
-      objective = objective + ...
-                  pho*(trace((Theta_old(:, :, i) + epsilon_rank*eye(N))\Theta(:, :, i)) + ...
-                       trace((Kappa_old(:, :, i) + epsilon_rank*eye(L))\Kappa(:, :, i))) + ...
-                  tau*wx(:, i)'*norms(Z(:, :, i), 2, 2);
-    end
+  pho = 1;
+  tau = 0.5;
+  objective = 0;
+  for i = 1:numGraphs
+    objective = objective + ...
+      pho*(trace((Theta_old(:, :, i) + epsilon_rank*eye(N))\Theta(:, :, i)) + ...
+      trace((Kappa_old(:, :, i) + epsilon_rank*eye(L))\Kappa(:, :, i))) + ...
+      tau*wx(:, i)'*norms(Z(:, :, i), 2, 2);
+  end
 
-    minimize(objective);
+  minimize(objective);
 
-    subject to
-      for i = 1:numGraphs
-        [Theta(:, :, i) Z(:, :, i); Z(:, :, i)' Kappa(:, :, i)] == semidefinite(N+L);
-      end
+  subject to
+  for i = 1:numGraphs
+    [Theta(:, :, i) Z(:, :, i); Z(:, :, i)' Kappa(:, :, i)] == semidefinite(N+L);
+  end
 
-      eq_constraint = 0;
-      for i = 1:numGraphs
-        eq_constraint = eq_constraint + V(:, :, i)*A{i}*vec(Z(:, :, i));
-      end
+  eq_constraint = 0;
+  for i = 1:numGraphs
+    eq_constraint = eq_constraint + V(:, :, i)*A{i}*vec(Z(:, :, i));
+  end
 
-      y == eq_constraint;
+  y == eq_constraint;
   cvx_end
 
   if verbose
@@ -79,7 +79,7 @@ while (flag == 1 && iter <= max_iter)
     b = 0;
     for i = 1:numGraphs
       a = a + pho*(trace((Theta_old(:, :, i) + epsilon_rank*eye(N))\Theta(:, :, i)) + ...
-                   trace((Kappa_old(:, :, i) + epsilon_rank*eye(L))\Kappa(:, :, i)));
+        trace((Kappa_old(:, :, i) + epsilon_rank*eye(L))\Kappa(:, :, i)));
       b = b + tau*wx(:, i)'*norms(Z(:, :, i), 2, 2);
     end
 
@@ -87,12 +87,12 @@ while (flag == 1 && iter <= max_iter)
   end
 
   if isempty(strfind(cvx_status, 'Solved'))
-      fname = sprintf('failed_problem_multigraph_bss_logdet_v%s', ...
-                      datestr(now, 'ddmmyyyyHHMMSS'));
-      warning(sprintf('cvx_status not Solved, saving %s.', fname))
-      save(fname)
-      Z_hat = nan(size(Z));
-      return
+    fname = sprintf('failed_problem_multigraph_bss_logdet_v%s', ...
+                    datestr(now, 'ddmmyyyyHHMMSS'));
+    warning(sprintf('cvx_status not Solved, saving %s.', fname))
+    save(fname)
+    Z_hat = nan(size(Z));
+    return
   end
 
   difference = 0;
