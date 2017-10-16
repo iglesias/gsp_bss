fd40c6028c92a6f296a9b6b23cc1008d647ec241
Fernando Iglesias Garcia <f.iglesias@scisports.com>
2017-10-16 22:47:55 +0200

    Add multigraph results figure and script with normal data.

---
+++ play_multigraph_bss_logdet_mats/multigraph_bss_logdet_normal_S_numGraphs_figure.m
@@ -0,0 +1,21 @@
+% Edit the last element to select number of columns / values of S.
+SS = 1:5;
+NUM_GRAPHS = 2:5;
+
+data_matrix = [0.91 0.79 0.75 0.57; ...
+               0.84 0.54 0.27 0.15; ...
+               0.74 0.33 0.12 0.02; ...
+               0.65 0.26 0.03 0.00; ...
+               0.51 0.15 0.01 0.00]';
+
+figure
+imagesc(data_matrix)
+axis xy
+xticks(SS)
+yticks(NUM_GRAPHS-1)
+yticklabels(NUM_GRAPHS)
+xlabel('S')
+ylabel('Number of graphs')
+colormap gray
+colorbar
+title('Rate of successful recovery with L=3 and N=100')
