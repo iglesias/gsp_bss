366f251a65c7dd9d5ea54e8debe79084a6b58bc1
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2018-01-06 15:57:48 +0100

    Minor. Remove outdated comment. Move input normalization for clarity.

--- multigraph_bss_gen_problem.m
+++ multigraph_bss_gen_problem.m
@@ -98,27 +98,23 @@ for i = 1:numGraphs
   truth.xSupport(i, :) = randperm(N, S);
 end
 
-%TODO generalize intersection from 2 graphs to numGraphs.
-% if isempty(intersect(truth.x1Support, truth.x2Support))
-%   fprintf('Intersection of the supports of the inputs empty.\n')
-% else
-%   fprintf('Intersection of the supports of the inputs non-empty.\n')
-% end
-
 truth.x = zeros(N, numGraphs);
 
 switch data_distribution
 case DataDistribution.Normal
   for i = 1:numGraphs
     truth.x(truth.xSupport(i, :), i) = randn(S, 1);
-    truth.x(:, i) = truth.x(:, i) ./ norm(truth.x(:, i));
   end
 
 case DataDistribution.Uniform
   for i = 1:numGraphs
     truth.x(truth.xSupport(i, :), i) = rand(S, 1);
-    truth.x(:, i) = truth.x(:, i) ./ norm(truth.x(:, i));
   end
+end
+
+% Normalize input signals.
+for i = 1:numGraphs
+  truth.x(:, i) = truth.x(:, i) ./ norm(truth.x(:, i));
 end
 
 y = H*truth.x(:);
