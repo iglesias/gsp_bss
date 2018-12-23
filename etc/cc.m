b92363dbe0f81ba1ef6f72f081d157117b8204fe
Fernando Iglesias-Garcia <fernando.iglesiasg@gmail.com>
2018-12-23 12:05:04 +0100

    WIP cross-coherence analysis with cycle and ER graphs.

---
+++ etc/cc.m
@@ -0,0 +1,45 @@
+function [erdosrenyi, cycle]  =cc
+
+% product of the norms of psi_2,l psi_1,l
+
+L = 3;
+
+N = 50;
+p = 0.1;
+for i = 1:2
+  erdosrenyi(i).A = generate_connected_ER(N, p);
+  [erdosrenyi(i).V, erdosrenyi(i).Lambda] = eig(erdosrenyi(i).A);
+%  erdosrenyi(i).U = inv(erdosrenyi(i).V);
+  erdosrenyi(i).Psi = make_Psi(diag(erdosrenyi(i).Lambda), L);
+  erdosrenyi(i).Psi_norms = reshape(norms(erdosrenyi(i).Psi'), N, 1);
+end
+
+shift = [-1 1];
+for i = 1:2
+  cycle(i).A = circshift(eye(N), shift(i));
+  [cycle(i).V, cycle(i).Lambda] = eig(cycle(i).A);
+  %cycle(i).U = inv(cycle(i).V);
+  cycle(i).Psi = make_Psi(diag(cycle(i).Lambda), L);
+  cycle(i).Psi_norms = reshape(norms(cycle(i).Psi'), N, 1);
+end
+
+figure
+x = erdosrenyi(1).Psi_norms .* erdosrenyi(2).Psi_norms;
+y = cycle(1).Psi_norms .* cycle(2).Psi_norms;
+figure(1)
+hist(x, 50)
+figure(2)
+hist(y, 50)
+
+end
+
+function Psi = make_Psi(lambda, L)
+
+assert(isvector(lambda))
+assert(isscalar(L))
+
+N = numel(lambda);
+
+Psi = repmat(lambda, 1, L).^repmat([0:L-1], N, 1);
+
+end
