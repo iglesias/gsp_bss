b9918719889bdd2f640f6812e3aad01c0bb2a356
Fernando J. Iglesias García <fernando.iglesiasg@gmail.com>
2019-12-17 13:19:01 +0100

    Update exploratory_DD.m

--- utils/exploratory_DD.m
+++ utils/exploratory_DD.m
@@ -27,3 +27,26 @@ for i = 1:n_graphs
 end
 
 % There are 10 graphs with 20 nodes.
+indices = find(NN == 20);
+i = indices(1); j = indices(2);
+
+y = XX{i} + XX{j};
+
+Psi_i = fliplr(vander(EE{i}));
+U_i = VV{i}';
+model.A{1} = kr(Psi_i', U_i')';
+Psi_j = fliplr(vander(EE{j}));
+U_j = VV{j}';
+model.A{2} = kr(Psi_j', U_j')';
+
+model.V = zeros(20, 20, 2); % two graphs/sources of 20 nodes.
+model.V(:,:,1) = VV{i};
+model.V(:,:,2) = VV{j};
+
+verbose_solver = false;
+Z_hat = multigraph_bss_logdet(y, model.A, model.V, verbose_solver)
+
+model.G(1).V = model.V(:,:,1);
+model.G(2).V = model.V(:,:,2);
+truth.Z = {SS{i}*(pinv(Psi_i)*HH_Freq{i}), SS{j}*(pinv(Psi_j)*HH_Freq{j})};
+multigraph_bss_print_summary(Z_hat, truth, model, y);
