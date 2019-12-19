5f3dbf05d92a8a232df281765d1d56e2029bbe20
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2019-12-19 09:07:11 +0100

    Experimenting.

--- utils/exploratory_DD.m
+++ utils/exploratory_DD.m
@@ -28,14 +28,18 @@ end
 
 % There are 10 graphs with 20 nodes.
 indices = find(NN == 20);
-i = indices(1); j = indices(2);
+i = indices(end); j = indices(end-1);
 
 y = XX{i} + XX{j};
 
-Psi_i = fliplr(vander(EE{i}));
+Psi_cut = 10;
+
+tmp_Psi_i = fliplr(vander(EE{i}));
+Psi_i = tmp_Psi_i(:, 1:Psi_cut);
 U_i = VV{i}';
 model.A{1} = kr(Psi_i', U_i')';
-Psi_j = fliplr(vander(EE{j}));
+tmp_Psi_j = fliplr(vander(EE{j}));
+Psi_j = tmp_Psi_j(:, 1:Psi_cut);
 U_j = VV{j}';
 model.A{2} = kr(Psi_j', U_j')';
 
@@ -43,10 +47,10 @@ model.V = zeros(20, 20, 2); % two graphs/sources of 20 nodes.
 model.V(:,:,1) = VV{i};
 model.V(:,:,2) = VV{j};
 
-verbose_solver = false;
-Z_hat = multigraph_bss_logdet(y, model.A, model.V, verbose_solver)
+verbose_solver = true;
+Z_hat = multigraph_bss_logdet(y, model.A, model.V, verbose_solver);
 
 model.G(1).V = model.V(:,:,1);
 model.G(2).V = model.V(:,:,2);
-truth.Z = {SS{i}*(pinv(Psi_i)*HH_Freq{i}), SS{j}*(pinv(Psi_j)*HH_Freq{j})};
+truth.Z = {SS{i}*(pinv(Psi_i)*HH_Freq{i})', SS{j}*(pinv(Psi_j)*HH_Freq{j})'};
 multigraph_bss_print_summary(Z_hat, truth, model, y);
