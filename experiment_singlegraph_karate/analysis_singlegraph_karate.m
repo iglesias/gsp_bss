e516862391d9385c9e5b59c6654d639d702080b9
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2020-01-07 08:35:14 +0100

    Add guard for NaNs before svd. Analysis script tweaks.

--- experiment_singlegraph_karate/analysis_singlegraph_karate.m
+++ experiment_singlegraph_karate/analysis_singlegraph_karate.m
@@ -1,15 +1,11 @@
-%SS = [1 2 3];
-%NUM_FILTERS = [2 3];
-%NOISE = [1e-6 1e-5 1e-4 1e-3 1e-2];
-
-SS = [1];
-NUM_FILTERS = 2;
-NOISE = [1e-6 1e-4 1e-2];
+SS = [1 2 3];
+NUM_FILTERS = [2 3];
+NOISE = [1e-6 1e-5 1e-4 1e-3 1e-2];
 
 num_plots = length(SS)*length(NUM_FILTERS);
 plot_mean_data = zeros(length(NOISE), num_plots);
 plot_median_data = zeros(length(NOISE), num_plots);
-fname_pattern = 'singlegraph_karate_S%d_numFilters%dnoise%d.mat';
+fname_pattern = 'singlegraph_karate_S%d_numFilters%dnoise%d';
 
 for n = 1:length(NOISE)
   plot_idx = 1;
@@ -17,10 +13,7 @@ for n = 1:length(NOISE)
     for numFilters = NUM_FILTERS
       fname = sprintf(fname_pattern, S, numFilters, NOISE(n));
       fprintf('Reading %s\n', fname);
-      load(fname)
-      SS = [1];
-      NUM_FILTERS = 2;
-      NOISE = [1e-6 1e-4 1e-2];
+      load(fname, '-mat')
 
       plot_mean_data(n, plot_idx) = mean(recovery_performance);
       plot_median_data(n, plot_idx) = median(recovery_performance);
@@ -31,16 +24,16 @@ for n = 1:length(NOISE)
 end
 
 figure
-semilogy(NOISE, plot_median_data, 's-')
-%legend('F = 2, S = 3', 'F = 3, S = 3', 'F = 2, S = 6', 'F = 3, S = 6', 'Location', 'NorthEast')
+loglog(NOISE, plot_median_data, 's-', 'LineWidth', 1.5)
+legend('F = 2, S = 1', 'F = 3, S = 1', 'F = 2, S = 2', 'F = 3, S = 2', 'F = 2, S = 3', 'F = 3, S = 3', 'Location', 'NorthEast')
 xlabel('\sigma_n')
 ylabel('Recovery performance (median)')
 grid on
 title('Karate network')
 
 figure
-semilogy(NOISE, plot_mean_data, 's-')
-%legend('F = 2, S = 3', 'F = 3, S = 3', 'F = 2, S = 6', 'F = 3, S = 6', 'Location', 'NorthEast')
+loglog(NOISE, plot_mean_data, 's-', 'LineWidth', 1.5)
+legend('F = 2, S = 3', 'F = 3, S = 3', 'F = 2, S = 6', 'F = 3, S = 6', 'F = 2, S = 3', 'F = 3, S = 3', 'Location', 'NorthEast')
 xlabel('\sigma_n')
 ylabel('Recovery performance (mean)')
 grid on
--- experiment_singlegraph_karate/singlegraph_karate_S_numFilters.m
+++ experiment_singlegraph_karate/singlegraph_karate_S_numFilters.m
@@ -1,11 +1,11 @@
 function singlegraph_karate_S_numFilters
 
 verbose_bss_logdet = false;
-num_simulations = 10;
+num_simulations = 100;
 
 params.L = 3;
 
-SS = [1 2 3];
+SS = [2 3];
 NUM_FILTERS = [2 3];
 NOISE = [1e-6 1e-5 1e-4 1e-3 1e-2];
 
@@ -13,7 +13,7 @@ for S = SS, for numFilters = NUM_FILTERS, for noise = NOISE
   tic
   success = zeros(num_simulations, 1);
   iters_to_solve = inf(num_simulations, 1);
-  recovery_performance = zeros(num_simulations, 1);
+  recovery_performance = inf(num_simulations, 1);
 
   params.S = S;
   params.numFilters = numFilters;
@@ -22,7 +22,11 @@ for S = SS, for numFilters = NUM_FILTERS, for noise = NOISE
   parfor n = 1:num_simulations
     [truth, model, y] = karate_bss_gen_problem(params);
     [Zsum_hat, iter] = bss_logdet_jointsum(y, model.A, model.G.V, verbose_bss_logdet);
-
+    
+    if any(vec(isnan(Zsum_hat)))
+      continue
+    end
+    
     [UZ, SZ, VZ] = svd(Zsum_hat, 'econ');
     Z_hat = zeros([size(Zsum_hat) numFilters]);
     for i = 1:numFilters
