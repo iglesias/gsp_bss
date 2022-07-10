610100d9591fb210b44f5d874f490171c5cc3a50
Fernando <nando95@msn.com>
2022-07-10 09:27:58 +0200

    Improve alpha_2 computation using TWO graphs (tried with BA graphs).

--- alpha_2_connected_BA.m
+++ alpha_2_connected_BA.m
@@ -3,13 +3,14 @@ function alpha_2s = alpha_2_connected_BA
 % Number of graph nodes.
 N = 50;
 % Number of graph realizations.
-num_mc = 50;
+num_mc = 1e1;
 % Number of non-zero nodes in the graph signal (sparsity strength).
 S = 1;
 assert(S <= N)
 % Number of filter taps.
 L = 1;
 assert(L <= N)
+fprintf('N=%d S=%d L=%d\n', N, S, L)
 
 alpha_2s = zeros(num_mc, 1);
 for i = 1:num_mc
@@ -18,12 +19,14 @@ end
 
 % disp(alpha_2s)
 
+fprintf('min=%.4f median=%.4f max=%.4f\n', min(alpha_2s), median(alpha_2s), max(alpha_2s))
+
 end
 
 function alpha_2 = work(N, S, L)
 
-% Adjacency matrix of a Barabábasi-Albert (BA) graph.
-model.G.W = generate_connected_BA(N, N/10);
+model.G(1).W = generate_connected_BA(N, N/10);
+model.G(2).W = generate_connected_BA(N, N/10);
 normalize_Psi = true;
 alpha_2 = compute_alpha_2(model, S, L, normalize_Psi);
 
--- compute_alpha_2.m
+++ compute_alpha_2.m
@@ -1,32 +1,47 @@
 function alpha_2 = compute_alpha_2(model, S, L, normalize_Psi)
 
-[model.G.V, Lambda] = eig(model.G.W);
-U = inv(model.G.V);
+numGraphs = 2;
+assert(length(model.G) == numGraphs)
 
-model.G.lambda = diag(Lambda);
-N = size(model.G.W, 1);
-assert(N == size(model.G.W, 2))
-
-model.Psi = repmat(model.G.lambda, 1, L).^repmat([0:L-1], N, 1); %#ok<NBRAK>
-assert(N == size(model.Psi, 1))
-
-if normalize_Psi
-  [Psi_svd_L, ~, ~] = svd(model.Psi, 0);
-  Psi = Psi_svd_L;
-else
-  Psi = model.Psi;
+for i = 1:numGraphs
+  assert(issymmetric(model.G(i).W))
+  [model.G(i).V, Lambda] = eig(model.G.W);
+  model.G(i).U = inv(model.G(i).V);
+  model.G(i).lambda = diag(Lambda);
 end
 
-kappa_U = kappaf(U, U, S, S);
-kappa_Psi = kappaf(Psi, Psi, L, L);
+N = size(model.G(1).W, 1);
+for i = 2:numGraphs
+  assert(N == size(model.G(i).W, 1))
+end
 
-rho_Psi1 = rhof(Psi, L);
-rho_Psi2 = rhof(Psi, L);
+for i = 1:numGraphs
+  model.Psi{i} = repmat(model.G(i).lambda, 1, L).^repmat([0:L-1], N, 1); %#ok<NBRAK>' 
+  assert(N == size(model.Psi{i}, 1))
+  
+  if normalize_Psi
+    [Psi_svd_L, ~, ~] = svd(model.Psi{i}, 0);
+    model.Psi{i} = Psi_svd_L;
+  end
+end
 
-rho_max = max(rhof(U, S), rhof(U, S));
+assert(numGraphs == 2)
 
-% Number of sources R.
-R = 2;
+U_1 = model.G(1).U;
+U_2 = model.G(2).U;
+
+Psi_1 = model.Psi{1};
+Psi_2 = model.Psi{2};
+
+kappa_U = kappaf(U_1, U_2, S, S);
+kappa_Psi = kappaf(Psi_1, Psi_2, L, L);
+
+rho_Psi1 = rhof(Psi_1, L);
+rho_Psi2 = rhof(Psi_2, L);
+
+rho_max = max(rhof(U_1, S), rhof(U_2, S));
+
+R = numGraphs;
 alpha_2 = 9/32 * 1/(rho_Psi1*rho_Psi2*rho_max + 1/R*kappa_U*kappa_Psi) * 1/(R^2*log10(2*N));
 
 end
